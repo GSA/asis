b0f59ce266fd7a2db20a8b82ef7d9b523c03c13a
Loren Siebert <loren@siebert.org>
2014-07-29 12:16:18 -0700

    Remove all the debugging output

--- spec/models/image_search_spec.rb
+++ spec/models/image_search_spec.rb
@@ -4,21 +4,10 @@ describe ImageSearch do
 
   context 'when relevant results exist in both Instagram and Flickr indexes' do
     before do
-      puts "***** #{InstagramPhoto.index_name} mapping before test:"
-      puts Elasticsearch::Persistence.client.indices.get_mapping(index: InstagramPhoto.index_name)
-      puts "Settings:"
-      puts Elasticsearch::Persistence.client.indices.get_settings(index: InstagramPhoto.index_name)
-
       FlickrPhoto.create(id: "photo1", owner: "owner1", profile_type: 'user', tags: [], title: "title1 earth", description: "desc 1", taken_at: Date.current, popularity: 100, url: "http://photo1", thumbnail_url: "http://photo_thumbnail1")
       FlickrPhoto.refresh_index!
       InstagramPhoto.create(id: "123456", username: 'user1', tags: %w(tag1 tag2), caption: 'first photo of earth', taken_at: Date.current, popularity: 101, url: "http://photo2", thumbnail_url: "http://photo_thumbnail2")
       InstagramPhoto.refresh_index!
-      puts "***** #{InstagramPhoto.index_name} documents after setup:"
-      puts InstagramPhoto.all
-      puts "Full info on instagram doc for this test: #{InstagramPhoto.find("123456")}"
-      puts "***** #{FlickrPhoto.index_name} documents after setup:"
-      puts FlickrPhoto.all
-      puts "Full info on flickr doc for this test: #{FlickrPhoto.find("photo1")}"
     end
 
     it 'should return results from both indexes' do
--- spec/support/test_services.rb
+++ spec/support/test_services.rb
@@ -2,25 +2,10 @@ module TestServices
   extend self
 
   def create_es_indexes
-    puts "Creating Elasticsearch test indexes...."
-    puts "#{InstagramPhoto.index_name} before create:"
-    begin
-      puts Elasticsearch::Persistence.client.indices.get_mapping(index: InstagramPhoto.index_name)
-      puts Elasticsearch::Persistence.client.indices.get_settings(index: InstagramPhoto.index_name)
-    rescue Elasticsearch::Transport::Transport::Errors::NotFound => i
-      puts "#{Rails.env}-oasis-flickr_photos does not yet exist"
-    end
     Dir[Rails.root.join('app', 'models', '*.rb')].map do |f|
       klass = File.basename(f, '.*').camelize.constantize
-      if klass.respond_to?(:create_index!)
-        puts "     (Re)creating index #{klass.to_s}"
-        klass.create_index!(force: true)
-      end
+      klass.create_index! if klass.respond_to?(:create_index!)
     end
-    puts "#{InstagramPhoto.index_name} mapping after create:"
-    puts Elasticsearch::Persistence.client.indices.get_mapping(index: InstagramPhoto.index_name)
-    puts "Settings:"
-    puts Elasticsearch::Persistence.client.indices.get_settings(index: InstagramPhoto.index_name)
   end
 
   def delete_es_indexes
