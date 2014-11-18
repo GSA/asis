70d50be1e7d6e6a643e4c7cc4ab94c9378883634
Loren Siebert <loren@siebert.org>
2014-11-18 13:12:11 -0800

    Extract synonyms into separate repo
    - They live in https://github.com/GSA/punchcard now
    
    Closes #2

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :application, 'oasis'
 set :repo_url, "git@github.com:GSA/oasis.git"
 set :rails_env, "production"
 set :deploy_to, "/home/search/oasis"
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets config/locales/analysis}
 set :linked_files, %w{config/airbrake.yml config/newrelic.yml config/flickr.yml config/sidekiq.yml config/instagram.yml config/secrets.yml config/elasticsearch.yml}
 set :bundle_binstubs, nil
 set :sidekiq_role, 'sidekiq'
