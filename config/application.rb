bc57e3377bc2a2885c708ecb8a59fc05b0d27727
John Maxwell <jmaxwell@fearless.tech>
2021-05-20 04:01:35 -0400

    Bumped rails defaults to 6.0
    Had to turn off zeitwerk. SRCH-2185 is the ticket to turn it back on.

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,11 @@ Bundler.require(*Rails.groups)
 module Oasis
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
+
+    # Turn on the classic autoloader, because zeitwerk blows
+    # up. SRCH-2185 is the ticket to move asis to zeitwerk.
+    config.autoloader = :classic
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
