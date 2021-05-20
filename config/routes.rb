b8a0e6021a9622de9a3851ac1cc6150d42e07ed5
John Maxwell <jmaxwell@fearless.tech>
2021-05-20 03:33:51 -0400

    Ran `bundle exec rails app:update` and merged changes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount API::Base => '/api'
   mount Sidekiq::Web => '/sidekiq'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
