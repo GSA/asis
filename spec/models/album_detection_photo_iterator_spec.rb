c23116a1d07f9f1b8a9ef0c975b6db4f0cdf9420
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2022-05-02 10:29:32 -0400

    SRCH-1840 remove code anticipating version conflicts - rescue and log StandardError when updating album name

--- spec/models/album_detection_photo_iterator_spec.rb
+++ spec/models/album_detection_photo_iterator_spec.rb
@@ -22,19 +22,4 @@ describe AlbumDetectionPhotoIterator, 'run' do
     expect(AlbumDetector).to(receive(:detect_albums!).exactly(5).times { [] })
     iterator.run
   end
-
-  describe 'assigning a default album' do
-    context 'when there is a version conflict during the update' do
-      let(:photo) { FlickrPhoto.create(id: 'photo1', owner: 'owner1', tags: [], title: 'title1 earth', description: 'desc 1', taken_at: Date.current, popularity: 100, url: 'http://photo1', thumbnail_url: 'http://photo_thumbnail1', album: 'album1', groups: []) }
-
-      before do
-        expect(photo).to receive(:update).and_raise Elasticsearch::Transport::Transport::Errors::Conflict
-      end
-
-      it 'logs a warning and continue' do
-        expect(Rails.logger).to receive(:warn)
-        AlbumDetector.detect_albums! photo
-      end
-    end
-  end
 end
