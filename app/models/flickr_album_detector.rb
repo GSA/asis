b7004b2c0d74027b25580199a597354824776897
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2022-10-06 11:57:10 -0400

    remove freeze

--- app/models/flickr_album_detector.rb
+++ app/models/flickr_album_detector.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class FlickrAlbumDetector < AlbumDetector
-  QUERY_FIELDS_THRESHOLD_HASH = { tags: 0.75, title: 0.5, description: 0.8 }.freeze
+  QUERY_FIELDS_THRESHOLD_HASH = { tags: 0.75, title: 0.5, description: 0.8 }
   FILTER_FIELDS = %w[owner groups taken_at].freeze
 
   def initialize(flickr_photo)
