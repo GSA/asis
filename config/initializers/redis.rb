8608d476847d77389c71da3b362dcdf67ef350cc
krbhavith <krbhavith@gmail.com>
2023-05-15 10:40:15 -0400

    fix sidekiq config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,5 +3,5 @@
 yaml = YAML.load_file("#{Rails.root}/config/sidekiq.yml")
 
 # rubocop:disable Style/GlobalVars
-$redis = Redis.new(url: yaml['url'], namespace: yaml['namespace'])
+$redis = Redis.new(url: yaml[Rails.env]['url'], namespace: yaml[Rails.env]['namespace'])
 # rubocop:enable Style/GlobalVars
