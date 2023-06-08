b1639153527cdaff647162c1e5a599482242d5a5
Bhavith Katpally <krbhavith@gmail.com>
2023-06-08 13:13:03 -0400

    SRCH-4121 Dockerize ASIS development (#209)

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
-yaml = YAML.load_file("#{Rails.root}/config/sidekiq.yml")
+if Rails.env.production?
+  sidekiq = YAML.load_file("#{Rails.root}/config/sidekiq.yml")
+else
+  sidekiq = Rails.configuration.sidekiq
+end
 
 # rubocop:disable Style/GlobalVars
-$redis = Redis.new(url: yaml['url'], namespace: yaml['namespace'])
+$redis = Redis.new(url: sidekiq['redis_url'], namespace: sidekiq['namespace'])
 # rubocop:enable Style/GlobalVars
