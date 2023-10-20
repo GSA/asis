90d30582ef74842f40d8f71d35a9fc9e5ce40f54
Darius Dzien <dariusz.dzien@gsa.gov>
2023-10-20 11:16:40 -0500

    SRCH-4593 Disable Elasticsearch discovery (#227)

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -2,7 +2,7 @@
 
 ES_CONFIG = Rails.application.config_for(:elasticsearch).freeze
 
-Elasticsearch::Persistence.client = Elasticsearch::Client.new(ES_CONFIG.merge({ randomize_hosts: true, retry_on_failure: true, reload_connections: true}))
+Elasticsearch::Persistence.client = Elasticsearch::Client.new(ES_CONFIG.merge({ randomize_hosts: true, retry_on_failure: true, reload_connections: false, reload_on_failure: false }))
 
 if Rails.configuration.elasticsearch['log']
   logger = ActiveSupport::Logger.new("log/#{Rails.env}.log")
