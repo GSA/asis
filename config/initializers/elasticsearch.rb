4e5d7fec895a49507b496e253207b8bdc6489b31
Luis Metzger <luismetzger@me.com>
2024-04-19 11:38:56 -0600

    SRCH-5084: Update SSL_VERIFY environment variable.

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -2,7 +2,15 @@
 
 ES_CONFIG = Rails.application.config_for(:elasticsearch).freeze
 
-Elasticsearch::Persistence.client = Elasticsearch::Client.new(ES_CONFIG.merge({ randomize_hosts: true, retry_on_failure: true, reload_connections: false, reload_on_failure: false }))
+Elasticsearch::Persistence.client = Elasticsearch::Client.new(ES_CONFIG.merge({ randomize_hosts: true,
+                                                                                retry_on_failure: true,
+                                                                                reload_connections: false,
+                                                                                reload_on_failure: false,
+                                                                                transport_options: {
+                                                                                  ssl: {
+                                                                                    verify: ENV['SSL_VERIFY'] || 'false'
+                                                                                  }
+                                                                                } }))
 
 if Rails.configuration.elasticsearch['log']
   logger = ActiveSupport::Logger.new("log/#{Rails.env}.log")
