4e2d352f8ec65a13f74aeb14c52a43fca2bde23f
Luis Metzger <luismetzger@me.com>
2024-05-23 07:09:40 -0600

    Update sleep time in spec.

--- spec/sidekiq_cli_spec.rb
+++ spec/sidekiq_cli_spec.rb
@@ -10,7 +10,7 @@ describe 'sidekiq CLI' do
     # Kick off sidekiq, wait a bit, and make sure the output doesn't include errors.
     # It's slow, but appears to be the only way to detect errors outside the workers.
     errors = Open3.popen2e('bundle exec sidekiq') do |_stdin, stdout_and_stderr, wait_thread|
-      sleep 30
+      sleep 40
       Process.kill('KILL', wait_thread.pid)
       # certain errors are written to STDOUT, so we look at both STDOUT and STDERR
       stdout_and_stderr.select { |line| line.include?('ERROR') }
