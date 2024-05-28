221c1c3ed26c4495c9f4e6096d5f60dbb5b55795
Luis Metzger <luismetzger@me.com>
2024-05-28 12:11:42 -0600

    update spec.

--- spec/config/initializers/filter_parameter_logging_spec.rb
+++ spec/config/initializers/filter_parameter_logging_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 describe 'ActiveSupport::ParameterFilter' do
   let(:config) { Oasis::Application.config }
-  let(:parameter_filter) { ActiveSupport::ParameterFilter.new(config.filter_parameters) }
+  let(:param_filter) { ActiveSupport::ParameterFilter.new(config.filter_parameters) }
 
   it 'filters query from logs' do
     expect(config.filter_parameters).to match(array_including(:query))
