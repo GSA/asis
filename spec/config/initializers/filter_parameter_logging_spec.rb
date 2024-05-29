4eeb0c8f69c9792209805cef256ae4d7b23c3010
Luis Metzger <luismetzger@me.com>
2024-05-28 22:23:42 -0600

    Update failing test.

--- spec/config/initializers/filter_parameter_logging_spec.rb
+++ spec/config/initializers/filter_parameter_logging_spec.rb
@@ -7,7 +7,9 @@ describe 'ActiveSupport::ParameterFilter' do
   let(:parameter_filter) { ActiveSupport::ParameterFilter.new(config.filter_parameters) }
 
   it 'filters query from logs' do
-    pattern_found = config.filter_parameters.any? { |regex| regex =~ /query/i }
+    query_string = 'query'
+    regexes = config.filter_parameters.map { |param| param.is_a?(Regexp) ? param : Regexp.new(Regexp.escape(param.to_s), Regexp::IGNORECASE) }
+    pattern_found = regexes.any? { |regex| query_string.match?(regex) }
     expect(pattern_found).to be true
   end
 end
