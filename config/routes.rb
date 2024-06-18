d30120e8362dfc0d2c954ebdfd56a64788b8aedb
Steven Barragán Naranjo <850686+stevenbarragan@users.noreply.github.com>
2024-06-18 12:37:20 -0700

    SRCH-5255 Remove sidekiq ui (#265)
    Sidekiq ui will be handled by searchgov

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Api::Base => '/api'
-  mount Sidekiq::Web => '/sidekiq'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
