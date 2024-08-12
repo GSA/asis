f9629bc8ce38c2e19ef5277d813081f0bb672ba9
Steven Barragan Naranjo <me@steven.mx>
2024-08-12 16:14:47 -0700

    Link .env file

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,7 @@ set :puma_preload_app, false
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
+append :linked_files, '.env'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
