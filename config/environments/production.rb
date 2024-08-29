4d43597d7d342e0705cacaf5c9545ef42f446c88
Luis Metzger <luismetzger@me.com>
2024-08-29 11:27:07 -0600

    Update production environment config endpoint.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.hosts << "localhost:3300"
 
   # Skip DNS rebinding protection for the default health check endpoint.
-  config.host_authorization = { exclude: ->(request) { request.path == "/healthcheck" } }
+  config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
   config.rails_semantic_logger.format = :json
 end
