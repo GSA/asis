64a6638bc289c7c8126f363184c65f4d9165d603
Luis Metzger <luismetzger@me.com>
2024-09-04 19:08:16 -0600

    Remove sidekiq route.

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,6 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Api::Base => '/api'
-
-  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-    ActiveSupport::SecurityUtils.secure_compare(username, ENV['SIDEKIQ_USERNAME']) &
-      ActiveSupport::SecurityUtils.secure_compare(password, ENV['SIDEKIQ_PASSWORD'])
-  end if Rails.env.production?
-
-  mount Sidekiq::Web => '/sidekiq'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
