45c1783d075edde793e211d5195353eba0164f4e
Steven Barragan Naranjo <me@steven.mx>
2024-09-10 09:09:02 -0700

    Setup puma logs
    * as they were before

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,19 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.19.1"
 
-set :application,    'asis'
-set :branch,         'staging'
-set :bundle_roles,   :all
-set :deploy_to,      ENV.fetch('DEPLOYMENT_PATH')
-set :format,         :pretty
-set :puma_bind,      'tcp://0.0.0.0:3300'
-set :rails_env,      'production'
-set :rbenv_type,     :user
-set :repo_url,       'https://github.com/GSA/asis.git'
-set :user,           ENV.fetch('SERVER_DEPLOYMENT_USER', 'search')
-set :whenever_roles, :cron
+set :application,     'asis'
+set :branch,          'staging'
+set :bundle_roles,    :all
+set :deploy_to,       ENV.fetch('DEPLOYMENT_PATH')
+set :format,          :pretty
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_bind,       'tcp://0.0.0.0:3300'
+set :puma_error_log,  "#{release_path}/log/puma.error.log"
+set :rails_env,       'production'
+set :rbenv_type,      :user
+set :repo_url,        'https://github.com/GSA/asis.git'
+set :user,            ENV.fetch('SERVER_DEPLOYMENT_USER', 'search')
+set :whenever_roles,  :cron
 
 append :linked_files, '.env'
 append :linked_dirs,  'log', 'tmp'
