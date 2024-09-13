871d091d62f1482ce867d96a17018c2a26516cca
Steven Barragan Naranjo <me@steven.mx>
2024-09-13 14:28:25 -0700

    Let sidekiq use the default rails logger
    * That's different depending on the environment.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,7 +7,3 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDIS_HOST'] }
 end
-
-Sidekiq.configure_server do |config|
-  config.logger = Logger.new('/home/search/asis/shared/log/sidekiq.log')
-end
