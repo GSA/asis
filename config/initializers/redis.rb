78fb8228417b98148e729d13ad2ffc51ecece09e
luisgmetzger <148913056+luisgmetzger@users.noreply.github.com>
2024-09-18 15:12:42 -0600

    Udpate environment variable from REDIS_HOST to REDIS_SYSTEM_URL. (#275)
    Co-authored-by: Luis Metzger <luismetzger@me.com>

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-$redis = Redis.new(url: ENV.fetch('REDIS_HOST', 'localhost'), port: ENV.fetch('REDIS_PORT', '6379'))
+redis_url = ENV['REDIS_SYSTEM_URL'] || 'redis://localhost:6379'
+$redis = Redis.new(url: redis_url)
 
 # Log the Redis URL to confirm it's set correctly
 Rails.logger.info "Redis URL: #{ENV.fetch('REDIS_HOST', 'localhost')}"
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = { host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'] }
+  config.redis = { url: ENV['REDIS_SYSTEM_URL'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'] }
+  config.redis = { url: ENV['REDIS_SYSTEM_URL'] }
 end
