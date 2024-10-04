9e63424cfc99b1a5117bf79dcabf1296b5525a03
Luis Metzger <luismetzger@me.com>
2024-10-04 12:39:04 -0600

    sync branches with latest main.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_HOST'] }
+  config.redis = { url: ENV['REDIS_SYSTEM_URL'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_HOST'] }
+  config.redis = { url: ENV['REDIS_SYSTEM_URL'] }
 end
 
 Sidekiq.configure_server do |config|
